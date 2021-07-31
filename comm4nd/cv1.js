8530f23d683826a2ba4a10c07414cfbde4229991
arcadia <85562683+josue629@users.noreply.github.com>
2021-07-31 16:36:54 -0500

    Add files via upload

---
+++ comm4nd/cv1.js
@@ -0,0 +1,471 @@
+exports.wpp = (pushname) => {
+   return `╭━━━━━「 🔷 *𝙄𝙉𝙁𝙊 𝘿𝙊 𝘽𝙊𝙏* 🔷 」
+┃✧𝙲𝚁𝙸𝙰𝙳𝙾𝚁:ᬊ͜͡𝐒𝚲𝐂𝚯𝐋𝚵⛧᭄ 
+┃✦𝙿𝚁𝙴𝙵𝙸𝚇𝙾: ${prefix}
+┃✧𝙲𝚁𝙸𝙰𝙳𝙾𝚁:wa.me/5562993294732
+┃✦ɴ° ᴅᴇ ᴄᴏᴍᴀɴᴅᴏs:447
+┃✧ɪᴛsᴜᴋɪ ʙᴏᴛ
+┃✦𝙷𝙾𝚁𝙰𝚁𝙸𝙾: segunda a sexta
+╰━━━々
+╭━━━━━「🔷 *REDES SOCIAIS*🔷」
+┃✧ᴄᴀɴᴀʟ ᴅᴏ ᴄʀɪᴀᴅᴏʀ✧
+┃✦https://youtube.com/channel/UC7frIuDpAIo9smTMU3pMZaw
+┃✧ᴄʜᴀᴛ✧
+┃✦https://chat.whatsapp.com/DFYmbl6hp82HxrfR4fefuZ
+╰━━━々
+╭━━━━━「🔷 *NOVOS*🔷」
+┃✧${prefix}dono
+┃✦${prefix}gay1
+┃✧${prefix}hentai2
+┃✦${prefix}listabanidos
+┃✧${prefix}waifus
+┃✦${prefix}animeavatar
+┃✧${prefix}waifu
+┃✦${prefix}virarmp3
+┃✧${prefix}playv2
+┃✦${prefix}lolih
+┃✧${prefix}loli
+┃✦${prefix}ddd
+┃✧${prefix}img
+┃✦${prefix}imagem
+┃✧${prefix}image
+┃✦${prefix}%pau
+┃✧${prefix}%feio
+┃✦${prefix}%lindo
+┃✧${prefix}%gostoso
+┃✦${prefix}reiniciar
+┃✧${prefix}gloogle
+┃✦${prefix}url hentai
+┃✧${prefix}itsuki
+┃✦${prefix}sn
+┃✧${prefix}bodia
+┃✦${prefix}banoit
+┃✧${prefix}batarde
+┃✦${prefix}onich
+┃✧${prefix}death
+┃✦${prefix}morte
+┃✧${prefix}nino
+┃✦${prefix}miku
+┃✧${prefix}waifu
+┃✦${prefix}nomehentai
+┃✧${prefix}memesbr
+┃✦${prefix}neko
+┃✧${prefix}wallpaperneko
+┃✦${prefix}pornogay
+┃✧${prefix}topangolanos
+┃✦${prefix}cartas
+┃✧${prefix}bios
+┃✦${prefix}medirpau
+┃✧${prefix}burro
+┃✦${prefix}inteligente
+┃✧${prefix}piadasuja
+┃✦${prefix}google
+┃✧${prefix}hentai
+┃✦${prefix}femdom
+┃✧${prefix}bobbs
+┃✦${prefix}smallbobbs
+┃✧${prefix}feed
+┃✦${prefix}erok
+┃✧${prefix}hololewd
+┃✦${prefix}futanari
+┃✧${prefix}pokemon2
+╰━━━々
+╭━━━「🔷 *SEM PREFIX*🔷」
+┃✧diorge
+┃✧figneko
+┃✧marco
+┃✧polo
+┃✧oi
+┃✧nino
+┃✧cadebot
+┃✧corno
+┃✧tmnc
+┃✧vsfd
+┃✧fdñ
+╰━━━々
+╭━━━━━「🔷 *ADMS*🔷」
+┃✧${prefix}bemvindo [0/1]
+┃✧${prefix}welcome [0/1]
+┃✧${prefix}add 55xxx
+┃✧${prefix}adicionar 55xxx
+┃✧${prefix}promover @membro
+┃✧${prefix}rebaixar @adm
+┃✧${prefix}grupo abrir/fechar
+┃✧${prefix}mudarnome seu txt
+┃✧${prefix}mudardesc seu txt
+┃✧${prefix}mudarfoto
+┃✧${prefix}antiracismo  
+┃✧${prefix}antifake
+┃✧${prefix}antilink  
+┃✧${prefix}stickertag
+┃✧${prefix}deletar
+┃✧${prefix}listadmins
+┃✧${prefix}adms
+┃✧${prefix}donogrupo
+┃✧${prefix}listonline
+┃✧${prefix}linkgrupo
+┃✧${prefix}infogp
+┃✧${prefix}marcar
+┃✧${prefix}marcar2
+┃✧${prefix}info
+┃✧${prefix}listabloq
+┃✧${prefix}lofi
+┃✧${prefix}map 0000-0000 
+┃✧${prefix} ${prefix}mapa 0000-0000 
+╰━━━々
+╭━━━━━「🔷 *FIGU* 🔷」
+┃✧${prefix}sticker
+┃✧${prefix}attp
+┃✧${prefix}figanime
+┃✧${prefix}figshit
+┃✧${prefix}fgato
+┃✧${prefix}psticker 
+┃✧${prefix}ola
+┃✧${prefix}pedro
+┃✧${prefix}monkey
+┃✧${prefix}mec
+┃✧${prefix}medo
+┃✧${prefix}fdp
+┃✧${prefix}jorge
+┃✧${prefix}blz
+┃✧${prefix}cringe
+┃✧${prefix}lindo
+┃✧${prefix}dru
+┃✧${prefix}macaule
+┃✧${prefix}chekcep
+┃✧${prefix}sttc
+┃✧${prefix}ttp
+┃✧${prefix}ttp3
+┃✧${prefix}s
+┃✧${prefix}fig
+┃✧${prefix}figurinha
+┃✧${prefix}audio seu txt
+┃✧${prefix}emoji 
+┃✧${prefix}semoj
+┃✧${prefix}toimg marca fig
+┃✧${prefix}converter marca fig
+┃✧${prefix}txtdafoto
+┃✧${prefix}ocr
+┃✧${prefix}wait
+┃✧${prefix}pesquisar
+╰━━━々
+╭━━━━━「🔷 *IMAGENS* 🔷」
+┃✧${prefix}cachorro
+┃✧${prefix}porno
+┃✧${prefix}belledelphine
+┃✧${prefix}metadinha
+┃✧${prefix}auau
+┃✧${prefix}dog
+┃✧${prefix}pokemon
+┃✧${prefix}gato 
+┃✧${prefix}pato 
+┃✧${prefix}rato
+┃✧${prefix}picapau
+┃✧${prefix}picapau2
+┃✧${prefix}picapau3
+┃✧${prefix}picapau4
+┃✧${prefix}picapal5
+╰━━━々
+╭━━━━━「🔷 *HENTAIS*🔷」
+┃✧${prefix}femdom
+┃✧${prefix}bobbs
+┃✧${prefix}smallbobbs
+┃✧${prefix}feed
+┃✧${prefix}erok
+┃✧${prefix}hololewd
+┃✧${prefix}futanari
+╰━━━々
+╭━━━━━「🔷 *MEMES*🔷」
+┃✧${prefix}memes
+┃✧${prefix}memesbr
+┃✧${prefix}memes1
+┃✧${prefix}memes2
+┃✧${prefix}memes3
+┃✧${prefix}memes4
+┃✧${prefix}memes5
+┃✧${prefix}memes6
+┃✧${prefix}memeanime
+┃✧${prefix}memeanime2
+┃✧${prefix}memeanime3
+┃✧${prefix}memeanime4
+╰━━━々
+╭━━━━━「🔷 *LINKS*🔷」
+┃✧${prefix}tiktokstalk Nick da conta
+┃✧${prefix}fototiktok
+┃✧${prefix}ytbaixa link
+╰━━━々
+╭━━━━━「🔷 *PREMIUM*🔷」
+┃✧${prefix}clearall
+┃✧${prefix}limpar
+┃✧${prefix}bloquear @membro
+┃✧${prefix}des
+┃✧${prefix}addprem
+┃✧${prefix}dellprem
+┃✧${prefix}recado
+┃✧${prefix}statustext
+┃✧${prefix}kitar
+┃✧${prefix}sigla nova sigla
+┃✧${prefix}setprefix
+┃✧${prefix}bc
+┃✧${prefix}bcgc
+┃✧${prefix}clonar
+┃✧${prefix}clone
+╰━━━々
+╭━━━━━「🔷 *JOGOS*🔷」
+┃✧${prefix}dado
+┃✧${prefix}jogardado
+┃✧${prefix}repetir
+┃✧${prefix}tapa @marque
+┃✧${prefix}beijar @marque
+┃✧${prefix}beijar2 @marque
+┃✧${prefix}dartapa @marque
+┃✧${prefix}darbeijo @marque
+┃✧${prefix}emojis
+┃✧${prefix}casino
+┃✧${prefix}casino2
+┃✧${prefix}jogoroleta
+┃✧${prefix}namorar @marque
+┃✧${prefix}viado @marque
+┃✧${prefix}baiano @marque
+┃✧${prefix}amor @marque
+┃✧${prefix}hetero @marque
+┃✧${prefix}corno @marque
+┃✧${prefix}iludido @marque
+┃✧${prefix}paixao @marque
+┃✧${prefix}fatality 
+┃✧${prefix}hackear 
+┃✧${prefix}kill @marque
+┃✧${prefix}cobrar @marque
+┃✧${prefix}casal
+┃✧${prefix}nazista
+┃✧${prefix}punheteiro
+┃✧${prefix}top10gados 
+┃✧${prefix}gados 
+┃✧${prefix}gays 
+┃✧${prefix}eusou
+┃✧${prefix}testecovid
+┃✧${prefix}heteros 
+┃✧${prefix}fakemsg 
+┃✧${prefix}gostosas 
+┃✧${prefix}gostosa
+┃✧${prefix}gay @marque
+┃✧${prefix}gado @marque
+╰━━━々
+╭━━━━━「🔷 *MUSICA*🔷」
+┃✧${prefix}musica
+┃✧${prefix}dmp3
+┃✧${prefix}play
+┃✧${prefix}spotify
+┃✧${prefix}spotifysearch
+┃✧${prefix}buscaryt
+┃✧${prefix}letra
+╰━━━々
+╭━━━━━「🔷 *MAIS*🔷」
+┃✧${prefix}infocorona
+┃✧${prefix}ebot
+┃✧${prefix}covidbr
+┃✧${prefix}siri
+┃✧${prefix}speed
+┃✧${prefix}tempo
+┃✧${prefix}ssweb
+┃✧${prefix}clima
+┃✧${prefix}conselho
+┃✧${prefix}criarbot
+┃✧${prefix}botstatus
+┃✧${prefix}termux
+┃✧${prefix}meme
+┃✧${prefix}memarque
+┃✧${prefix}gerarnick
+┃✧${prefix}wallpaper
+┃✧${prefix}bomdia
+┃✧${prefix}boanoite
+┃✧${prefix}camuflar
+┃✧${prefix}roleta
+┃✧${prefix}vida
+┃✧${prefix}wa.me
+┃✧${prefix}perfil
+┃✧${prefix}meucopyright
+┃✧${prefix}pinterest
+┃✧${prefix}kpop ou ${prefix}bts
+┃✧${prefix}twitter
+┃✧${prefix}banir
+╰━━━々
+╭━━━━━「🔷 *PRESENTES*🔷」
+┃✧${prefix}malkova
+┃✧${prefix}malkova2
+┃✧${prefix}miamalkova
+┃✧${prefix}belle
+┃✧${prefix}belle2
+┃✧${prefix}belle3
+┃✧${prefix}belle4
+┃✧${prefix}mia
+┃✧${prefix}mia2
+┃✧${prefix}mia3
+╰━━━々
+╭━━━━━「🔷 *LOGOS*🔷」
+┃✧${prefix}gtav 
+┃✧${prefix}face 
+┃✧${prefix}contagem
+┃✧${prefix}simih 
+┃✧${prefix}simi
+┃✧${prefix}homem
+┃✧${prefix}mulher
+┃✧${prefix}imgnetflix 
+┃✧${prefix}nomesig
+┃✧${prefix}tiktok
+┃✧${prefix}tiktokstalk
+┃✧${prefix}moddroid 
+┃✧${prefix}happymod 
+┃✧${prefix}github 
+╰━━━々
+╭━━━━━「🔷 *VIDEO*🔷」
+┃✧${prefix}say1
+┃✧${prefix}say2
+┃✧${prefix}say3
+┃✧${prefix}say4
+┃✧${prefix}edit1
+┃✧${prefix}edit2
+┃✧${prefix}edit3
+┃✧${prefix}edit4
+┃✧${prefix}xxx1
+┃✧${prefix}xxx2
+┃✧${prefix}teto
+┃✧${prefix}teto2
+┃✧${prefix}teto3
+┃✧${prefix}shit
+┃✧${prefix}shit2
+┃✧${prefix}shit3
+╰━━━々
+╭━━━━━「🔷 *AUDIO*🔷」
+┃✧${prefix}bv
+┃✧${prefix}grave
+┃✧${prefix}normal
+┃✧${prefix}slow
+┃✧${prefix}diabolico
+┃✧${prefix}esquilo
+┃✧${prefix}tupai
+┃✧${prefix}bot
+┃✧${prefix}saiu
+┃✧${prefix}cantar
+┃✧${prefix}hylander
+┃✧${prefix}akdoflamengo 
+┃✧${prefix}glub
+┃✧${prefix}m4
+┃✧${prefix}banhodeleite
+┃✧${prefix}beat
+┃✧${prefix}kalidade
+┃✧${prefix}banido
+┃✧${prefix}banido2
+┃✧${prefix}macacaco
+┃✧${prefix}mcbrinquedo
+┃✧${prefix}audiot
+┃✧${prefix}futuro
+┃✧${prefix}nobru
+┃✧${prefix}peppa
+┃✧${prefix}fortnite
+┃✧${prefix}soldado
+╰━━━々
+╭━━━━━「🔷 *GERADOR* 🔷」
+┃✧${prefix}gpessoa
+┃✧${prefix}gbin
+┃✧${prefix}gcpf
+┃✧${prefix}gerarpessoa
+┃✧${prefix}gerarcartao
+┃✧${prefix}geraridentidade
+┃✧${prefix}geradorcpf
+┃✧${prefix}geradorcnpj
+┃✧${prefix}geradorcns
+┃✧${prefix}destrava
+┃✧${prefix}bin
+┃✧${prefix}cnpj
+┃✧${prefix}iplog
+╰━━━々
+╭━━━━━「🔷 *EFEITOS DE FIGU* 🔷」
+┃✧${prefix}ger   (Marque a Figurinha)
+┃✧${prefix}lgbt   (Marque a Figurinha)
+┃✧${prefix}figcinza (Marque a Figurinha)
+┃✧${prefix}figvermelha (Marque a Figurinha)
+┃✧${prefix}picture
+┃✧${prefix}sfire  (Marque a Figurinha)
+┃✧${prefix}laptop  (Marque a Figurinha)
+┃✧${prefix}nightbeach (Marque a Figurinha)
+┃✧${prefix}wasted  (Marque a Figurinha)
+┃✧${prefix}drawing  (Marque a Figurinha)
+┃✧${prefix}addsticker
+┃✧${prefix}stickerlist
+┃✧${prefix}getsticker
+┃✧${prefix}gplaystore _Nome do app_
+┃✧${prefix}apkpure _Nome do app
+┃✧${prefix}Instagram
+╰━━━々
+╭━━━━━「🔷 *+LOGOS* 🔷」
+┃✧${prefix}plogo (texto)
+┃✧${prefix}plaquinha (texto)
+┃✧${prefix}plaquinhas2 (texto)
+┃✧${prefix}plaquinhas3 (texto)
+┃✧${prefix}plaquinhas4 (texto)
+┃✧${prefix}conquista
+┃✧${prefix}trump
+┃✧${prefix}gta
+┃✧${prefix}logogalaxy
+┃✧${prefix}logojogo
+┃✧${prefix}clogo (texto)
+┃✧${prefix}slogo (texto)
+┃✧${prefix}pslogo (texto)
+┃✧${prefix}hublogo (texto)
+┃✧${prefix}logohub
+┃✧${prefix}steel texto/texto
+┃✧${prefix}efeitin texto/texto
+┃✧${prefix}efeitin2 (texto)
+┃✧${prefix}blogo (texto)
+┃✧${prefix}bologo (texto)
+┃✧${prefix}milogo (texto)
+┃✧${prefix}black (texto)
+┃✧${prefix}leaologo texto/texto
+┃✧${prefix}logoplaca (texto)
+┃✧${prefix}googlelogo (texto)
+┃✧${prefix}logo3d seu txt
+┃✧${prefix}dnulis (texto)
+┃✧${prefix}grafit texto/texto
+┃✧${prefix}letext (texto)
+┃✧${prefix}matrix (texto)
+┃✧${prefix}neonlogo (texto)
+┃✧${prefix}codlogo (texto)
+┃✧${prefix}praialogo (texto)
+┃✧${prefix}artificiologo (texto)
+┃✧${prefix}qrcodigo 
+┃✧${prefix}logotxt (texto)
+┃✧${prefix}fflogo (texto)
+┃✧${prefix}logobanner (texto)
+┃✧${prefix}logochuva (texto)
+┃✧${prefix}dnobg (texto)
+┃✧${prefix}dnobg2 teste/teste
+╰━━━々
+╭━━━━━「🔷 *ANIMES*🔷」
+┃✧${prefix}animecry
+┃✧${prefix}buscar
+┃✧${prefix}naruto
+┃✧${prefix}boruto
+┃✧${prefix}minato
+┃✧${prefix}sasuke
+┃✧${prefix}hinata
+┃✧${prefix}sakura
+┃✧${prefix}anime
+┃✧${prefix}kiss
+┃✧${prefix}kissgif
+┃✧${prefix}lolifig
+┃✧${prefix}hentaifig
+┃✧${prefix}shota
+┃✧${prefix}nekofig
+┃✧${prefix}hentai
+┃✧*atençao atençao❗..meu pau em tua mao✋*
+╰━━━々
+╭━━━━━「🔷 *SUPORTE*🔷」
+┃✧${prefix}criador
+┃✧${prefix}info
+┃✧${prefix}dono
+┃✧${prefix}bug
+┃✧${prefix}reportarbug
+╰━━━々
+`
+}
---
+++ comm4nd/cv2.js
@@ -0,0 +1,25 @@
+ exports.wpp = (pushname) => {
+   return `╓───「♦️ *NOVOS*♦️」
+│✧${prefix}dono
+│✧${prefix}gay1
+│✧${prefix}hentai2
+│✧${prefix}listabanidos
+│✧${prefix}waifus
+│✧${prefix}animeavatar
+│✧${prefix}waifu
+│✧${prefix}virarmp3
+│✧${prefix}playv2
+│✧${prefix}lolih
+│✧${prefix}loli
+│✧${prefix}ddd
+│✧${prefix}img
+│✧${prefix}imagem
+│✧${prefix}image
+│✧${prefix}%pau
+│✧${prefix}%feio
+│✧${prefix}%lindo
+│✧${prefix}%gostoso
+│✧${prefix}reiniciar
+╙───々
+`
+}
